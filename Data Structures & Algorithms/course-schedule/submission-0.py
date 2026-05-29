5e962b14a6c1e738a251856a8ae9de618bb2935f
Saurabh Marne <40160235+saurabhmarne9595@users.noreply.github.com>
2026-05-29 16:41:44 -0500

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, val):
+        val = val
+        children = []
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        added = {i:[] for i in range(numCourses)}
+        for cur, pre in prerequisites:
+            if not cur in added:
+                added[cur] = []
+            added[cur].append(pre)
+
+        visiting = set()
+
+        def dfs(cur):
+            if cur in visiting:
+                return False
+            if added[cur] == []:
+                return True
+            visiting.add(cur)
+            for n in added[cur]:
+                if not dfs(n):
+                    return False
+            visiting.remove(cur)
+            return True
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
