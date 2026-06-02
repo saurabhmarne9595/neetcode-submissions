1159c090469e0e4f98045a85230c6241c458fa84
Saurabh Marne <40160235+saurabhmarne9595@users.noreply.github.com>
2026-06-02 11:10:35 -0500

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,11 @@
+from functools import lru_cache
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @lru_cache(None)
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+
+            return max(nums[i] + dfs(i + 2), dfs(i + 1))
+
+        return dfs(0)
